6bc071812765accd90d9de6dfbfa7b9c8b1ba7ad
Tumi Moeng <39015796+DarkPr0digy@users.noreply.github.com>
2023-08-23 16:30:07 +0200

    Henry Initialization + Personalities Config File

---
+++ Henry.py
@@ -0,0 +1,29 @@
+import json
+from Chatbot import Chatbot
+
+
+class Henry(Chatbot):
+    def __init__(self):
+        # Get Introductory Message
+        with open("personalities.json") as intro_file:
+            config = json.load(intro_file)
+
+        intro_message = config["starting_message"]["henry"]
+        characteristic = config["characteristic"]["henry"]
+
+        super().__init__(name="Henry", introduction=intro_message)
+
+        self.messages.extend([{"role": "system", "content": characteristic},
+                             {"role": "assistant", "content": intro_message}])
+
+
+if __name__ == "__main__":
+    chatbot = Henry()
+
+    print(f"{chatbot.name}: {chatbot.get_start_message()}")
+
+    while True:
+        user_input = input("User: ")
+
+        response = chatbot.generate_response(user_input)
+        print(f"{chatbot.name}: {response}")
