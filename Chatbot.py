6575e3e58b36219f859b57a6291d16918fab73f6
Tumi Moeng <39015796+DarkPr0digy@users.noreply.github.com>
2023-08-23 15:53:47 +0200

    Implemented Response Generation

--- Chatbot.py
+++ Chatbot.py
@@ -12,13 +12,24 @@ class Chatbot:
         with open("config.json") as config_file:
             config = json.load(config_file)
 
-        # Test
         self.api_key = config["api_keys"]["open_ai"]
-
         openai.api_key = self.api_key
 
+        self.messages = []
+
     def generate_response(self, user_input: str):
-        raise NotImplementedError("Subclasses must implement this method.")
+        self.messages.append({"role": "user", "content": user_input})
+
+        conversation = openai.ChatCompletion.create(
+            model="gpt-3.5",
+            messages=self.messages,
+            temperature=0.9)
+
+        response = conversation.choices[0].message.content
+
+        self.messages.append({"role": self.name, "content": response})
+
+        return response
 
     def get_start_message(self):
         return self.introduction
