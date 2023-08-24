e538d35d6ece65bd02365c5e8a42fe1e0f4e6805
Tumi Moeng <39015796+DarkPr0digy@users.noreply.github.com>
2023-08-24 11:34:01 +0200

    Code refactorization for reusability

--- Chatbot.py
+++ Chatbot.py
@@ -3,9 +3,10 @@ import openai
 
 
 class Chatbot:
-    def __init__(self, name: str, introduction: str):
+    def __init__(self, name: str):
         self.name = name
-        self.introduction = introduction
+        self.introduction = None
+        self.characteristic = None
 
         # GPT API Key
         with open("config.json") as config_file:
@@ -15,6 +16,23 @@ class Chatbot:
         openai.api_key = self.api_key
 
         self.messages = []
+
+    def load_personality(self, personality_name: str):
+        with open("personalities.json") as personality_file:
+            personalities = json.load(personality_file)
+
+        personality_data = personalities.get(personality_name)
+
+        if personality_data:
+            self.introduction = personalities[personality_name]["starting_message"]
+            self.characteristic = personalities[personality_name]["characteristic"]
+        else:
+            raise ValueError(f"Personality {personality_name} not found")
+
+    def introduce(self):
+        self.messages.extend([{"role": "system", "content": self.characteristic},
+                              {"role": "assistant", "content": self.introduction}])
+        print(f"{self.name}: {self.introduction}")
 
     def generate_response(self, user_input: str):
         if user_input == "EXIT":
@@ -32,17 +50,3 @@ class Chatbot:
         self.messages.append({"role": "assistant", "content": response})
 
         return response
-
-    def get_start_message(self):
-        return self.introduction
-
-
-if __name__ == "__main__":
-    chatbot = Chatbot(name="CovidBot", introduction="I am a chatbot")
-
-    while True:
-        user_input = input("User: ")
-
-        response = chatbot.generate_response(user_input)
-        print(f"{chatbot.name}: {response}")
-
