e768eaca74a9cb935545e1bbeccb1d3752ce9e7f
Tumi Moeng <39015796+DarkPr0digy@users.noreply.github.com>
2023-08-27 23:41:13 +0200

    Reports Completed and  comments added to code

--- Chatbot.py
+++ Chatbot.py
@@ -4,9 +4,12 @@ import os
 from datetime import datetime
 
 
-
 class Chatbot:
     def __init__(self, name: str):
+        """ Create a chatbot with a given personality
+        :param name: Name of the personality
+        """
+
         self.name = name
         self.introduction = None
         self.characteristic = None
@@ -14,7 +17,7 @@ class Chatbot:
         self.user_total_characters = 0
         self.chatbot_total_words = 0
 
-        # GPT API Key
+        # Load API Key from Config File
         with open("config.json") as config_file:
             config = json.load(config_file)
 
@@ -23,10 +26,15 @@ class Chatbot:
 
         self.messages = []
 
+        # Load Personality and Introduce Chatbot
         self._load_personality(name)
         self._introduce()
 
     def _load_personality(self, personality_name: str):
+        """ Load the personality from the personalities.json file
+        :param personality_name: Name of the personality
+        """
+
         with open("personalities.json") as personality_file:
             personalities = json.load(personality_file)
 
@@ -39,11 +47,17 @@ class Chatbot:
             raise ValueError(f"Personality {personality_name} not found")
 
     def _introduce(self):
+        """ Introduce the chatbot to the user
+        """
         self.messages.extend([{"role": "system", "content": self.characteristic},
                               {"role": "assistant", "content": self.introduction}])
         print(f"{self.name}: {self.introduction}")
 
     def generate_response(self, user_input: str):
+        """ Generate a response to the user input
+        :param user_input: Input from the user
+        :return: Response from the chatbot"""
+
         if user_input.lower() == "exit":
             self._conversational_insights()
             return "See you next time"
@@ -59,35 +73,46 @@ class Chatbot:
 
         self.messages.append({"role": "assistant", "content": response})
 
+        # Update Counts
         self.user_total_characters += len(user_input)
         self.chatbot_total_words += len(response.split(" "))
 
         return response
 
     def _conversational_insights(self):
+        """ Generate conversational insights and save them to a file
+        """
         self.messages.append(
             {"role": "system", "content": "Generate a python formatted dictionary containing the topic of our "
                                           "conversation based on the users input, and if it cannot be determined return UNKNOWN, with its key being 'topic'. Additionally, the name of the user if it can "
-                                          "be determined, if not return UNKNOWN, with its key being 'user_name'"})
+                                          "be determined, if not return UNKNOWN, with its key being 'user_name'. Send only the dictionary as a string."})
 
         conversation = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=self.messages,
             temperature=0.9)
 
+        # Try get response and convert to dictionary
         response = conversation.choices[0].message.content
+        # response = eval(response)
 
-        print(response)
+        try:
+            response_dict = json.loads(response)
+            print(response_dict)
+            topic = response_dict.get('topic')
+            user_name = response_dict.get('user_name')
+        except:
+            topic = "UNKNOWN"
+            user_name = "UNKNOWN"
 
-        response = eval(response)
 
         timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
         conversation_meta_information = {
             "Name of chatbot": self.name,
             "Number of characters typed by user": self.user_total_characters,
             "Number of words used by chatbot": self.chatbot_total_words,
-            "Subject of conversation": response['topic'],
-            "Name of user": response['user_name']}
+            "Subject of conversation": topic,
+            "Name of user": user_name}
 
         conversation_data = {
             "meta-data": conversation_meta_information,
--- ConversationManager.py
+++ ConversationManager.py
@@ -3,22 +3,32 @@ from Chatbot import Chatbot
 
 
 class ConversationManager:
+    """A class to manage conversations and personality selection"""
     def __init__(self):
+        """
+        Create a conversation manager instance
+        """
         # Determine Personality User Wants to Chat With
         with open("personalities.json") as personality_file:
             personalities = json.load(personality_file)
 
         self.available_personalities = list(personalities.keys())
-        if len(self.available_personalities) > 1:
-            print('With whom would you like to chat today?')
-            for i, personality in enumerate(self.available_personalities):
-                print(f'[{i}] {personality}')
+        print('With whom would you like to chat today?')
+        for i, personality in enumerate(self.available_personalities):
+            print(f'[{i}] {personality}')
 
-    def select_personality(self, personality_index: int):
+    def select_personality(self, personality_index):
+        """Select a personality to chat with based on the index
+        :param personality_index: Index of the personality to chat with
+        :return: Chatbot instance of the selected personality
+        """
         try:
-            return Chatbot(self.available_personalities[personality_index])
+            return Chatbot(self.available_personalities[int(personality_index)])
         except IndexError:
             print('Invalid personality index, please try again')
+            return None
+        except ValueError:
+            print('Personalities must be entered as an int, please try again')
             return None
 
 
@@ -27,7 +37,7 @@ if __name__ == "__main__":
     selected_personality = None
     while not selected_personality:
         user_input = input("User: ")
-        selected_personality = conversation_manager.select_personality(int(user_input))
+        selected_personality = conversation_manager.select_personality(user_input)
 
     while True:
         user_input = input("User: ")
